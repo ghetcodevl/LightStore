2a57d92f87a3728300cffa0c571e23babd93a9f4
miinnnnnnn <kiemtienkhong241@gmail.com>
2026-04-08 05:00:20 +0700

    fist commit

---
+++ src/java/model/ProductDAO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+/**
+ *
+ * @author lttru
+ */
+public class ProductDAO {
+   public static List<Product> getAll() throws ClassNotFoundException, SQLException{
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM products";
+        try(Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()){
+            while(rs.next()){
+                list.add(new Product(
+                    rs.getInt("id"),
+                    rs.getString("name"),                    
+                    rs.getString("image"),
+                    rs.getDouble("price")));             
+            }
+        }
+        return list;
+    } 
+}
