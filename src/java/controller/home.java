c51583e2b399973801879615ef8bcbce93ae66d7
miinnnnnnn <kiemtienkhong241@gmail.com>
2026-04-08 13:05:54 +0700

    fist commit

--- src/java/controller/home.java
+++ src/java/controller/home.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Product;
-import model.ProductDAO;
+import dao.ProductDAO;
 
 /**
  *
--- src/java/dao/ProductDAO.java
+++ src/java/dao/ProductDAO.java
@@ -2,11 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package dao;
 
+import utils.DBConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
+import model.Product;
 
 /**
  *
